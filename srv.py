47746fa3b72bc35e7c1cf8f201377260e8c3612e
i.chernykh <chernykh@ocas.ai>
2024-01-22 16:33:32 +0500

    finish service

--- srv.py
+++ srv.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from src.searcher.searcher import Searcher
+from query import Query
 
 app = FastAPI()
 
@@ -14,7 +15,7 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/question/{query}")
-async def read_item(query):
-    resp = await searcher.request(query)
+@app.post("/question")
+async def read_item(query: Query):
+    resp = await searcher.request(query.text)
     return {"text": resp}
